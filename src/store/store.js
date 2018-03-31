5d6684fbdee779bd2f71d1f06dd5fd1e8377d045
Michael Cruz <michael@michaelcruz.io>
2018-03-30 17:47:38 -0700

    initial commit

---
+++ src/store/store.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import counter from './modules/counter';
+
+import * as actions from './actions';
+import * as getters from './getters';
+import * as mutations from './mutations';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    counter: 0,
+    value: 0
+  },
+  getters,
+  mutations,
+  actions,
+  modules: {
+    counter
+  }
+});
